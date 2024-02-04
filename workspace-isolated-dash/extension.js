f06fa901eb206d48d4207ebf1c25d3793b834b6c
Yuki Osada <yuuki.k.osada@gmail.com>
2024-02-05 10:18:08 +1100

    Fix up styles and remove deprecated Lang usage as per Gnome extension review

--- workspace-isolated-dash/extension.js
+++ workspace-isolated-dash/extension.js
@@ -1,20 +1,15 @@
-const Lang = imports.lang;
-
+const AppIcon = imports.ui.appDisplay.AppIcon;
+const Main = imports.ui.main;
 const Shell = imports.gi.Shell;
 
-const Main = imports.ui.main;
-const AppIcon = imports.ui.appDisplay.AppIcon;
+let _appSystem;
 
-const AppSystem = Shell.AppSystem.get_default();
-
-const WorkspaceIsolator = new Lang.Class({
-	Name: 'WorkspaceIsolator',
-
-	_init: function() {
+const WorkspaceIsolator = class WorkspaceIsolator {
+	constructor() {
 		// Extend AppSystem to only return applications running on the active workspace
-		AppSystem._workspace_isolated_dash_nyuki_get_running = AppSystem.get_running;
-		AppSystem.get_running = function() {
-			let running = AppSystem._workspace_isolated_dash_nyuki_get_running();
+		_appSystem._workspace_isolated_dash_nyuki_get_running = _appSystem.get_running;
+		_appSystem.get_running = function() {
+			let running = _appSystem._workspace_isolated_dash_nyuki_get_running();
 			if (Main.overview.visible) {
 				return running.filter(WorkspaceIsolator.isActiveApp);
 			} else {
@@ -45,13 +40,13 @@ const WorkspaceIsolator = new Lang.Class({
 		// - window created
 		// - window closed
 		this._onRestackedId = global.display.connect('restacked', WorkspaceIsolator.refresh);
-	},
+	}
 
-	destroy: function() {
+	destroy() {
 		// Revert the AppSystem function
-		if (AppSystem._workspace_isolated_dash_nyuki_get_running) {
-			AppSystem.get_running = AppSystem._workspace_isolated_dash_nyuki_get_running;
-			delete AppSystem._workspace_isolated_dash_nyuki_get_running;
+		if (_appSystem._workspace_isolated_dash_nyuki_get_running) {
+			_appSystem.get_running = _appSystem._workspace_isolated_dash_nyuki_get_running;
+			delete _appSystem._workspace_isolated_dash_nyuki_get_running;
 		}
 		// Revert the App function
 		if (Shell.App.prototype._workspace_isolated_dash_nyuki_activate) {
@@ -74,13 +69,16 @@ const WorkspaceIsolator = new Lang.Class({
 			this._onSwitchWorkspaceId = 0;
 		}
 	}
-});
+};
+
 // Check if an application is on the active workspace
 WorkspaceIsolator.isActiveApp = function(app) {
 	return app.is_on_workspace(global.workspaceManager.get_active_workspace());
 };
+
 // Refresh dash
 WorkspaceIsolator.refresh = function() {
+	const AppSystem = _appSystem || Shell.AppSystem.get_default();
 	// Update icon state of all running applications
 	let running;
 	if (AppSystem._workspace_isolated_dash_nyuki_get_running) {
@@ -91,22 +89,27 @@ WorkspaceIsolator.refresh = function() {
 	running.forEach(function(app) {
 		app.notify('state');
 	});
+
 	// Update applications shown in the dash
-	Main.overview._overview._controls.dash._queueRedisplay();
+	let dash = Main.overview._dash || Main.overview.dash;
+	dash._queueRedisplay();
 };
 
 let _wsIsolator;
 
+function init() {
+	/* do nothing */
+}
+
 function enable() {
+	_appSystem = Shell.AppSystem.get_default();
 	_wsIsolator = new WorkspaceIsolator();
 	WorkspaceIsolator.refresh();
 }
 
 function disable() {
 	_wsIsolator.destroy();
+	_wsIsolator = null;
 	WorkspaceIsolator.refresh();
-}
-
-function init(meta) {
-	/* do nothing */
+	_appSystem = null;
 }
